56b10ecd267f581b91cfd3690583c1a64d58f76a
jacc89 <laflacalex9@gmail.com>
2023-10-01 00:04:07 -0500

    creacion de modulos angular

---
+++ DoctorAppFrontend/src/app/compartidos/compartidos.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule , FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  exports:[
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule
+
+  ]
+})
+export class CompartidosModule { }
