0a22348e9d48e982bdfa4cffab8a05a3ec96b33b
jacc89 <laflacalex9@gmail.com>
2023-10-01 19:13:05 -0500

    Usuario

--- DoctorAppFrontend/src/app/usuarios/usuarios.module.ts
+++ DoctorAppFrontend/src/app/usuarios/usuarios.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CompartidosModule } from '../compartidos/compartidos.module';
+import { UsuarioService } from './Services/usuario.service';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    CompartidosModule
+  ],
+  providers:[
+    UsuarioService
   ]
 })
 export class UsuariosModule { }
